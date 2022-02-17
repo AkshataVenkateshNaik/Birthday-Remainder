6706cff9825ca36fa36505ce063d70f2647b2cbc
AkshataVenkateshNaik <akshatanaik5558@gmail.com>
2022-02-17 18:57:17 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Bertie Yates",
+    age: 29,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb2DFkEQQFyU0vPOZoUP4mMyMGEjYycTfb3A&usqp=CAU.jpg",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    age: 23,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7Xh9PifMRhzJfnv4DVRnhcFv1DsMB0RtcAQ&usqp=CAU.jpg",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    age: 31,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJptEHIpRF7cvSA7bqtrGD5hKw_e06yXxyqlEfZwtN9jPqiGYl7WUkx3gZDjBNK5hO-cg&usqp=CAU.jpg",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    age: 27,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Lola Gardner",
+    age: 26,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
